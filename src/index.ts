5554827dc50f9bafd5593928473d3f1911177fb9
Michael Moore <michaelmoore@Michaels-Mac-mini.local>
2025-01-14 10:27:07 -0500

    added vercel file, updated tsconfig and added api folder

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import app from './app'
 
-const port = process.env.PORT || 3000 
+const port = process.env.PORT || 3000
 
-app.listen(port, () => console.log(`server is running on port ${port}`)) 
+app.listen(port, () => console.log(`server is running on port ${port}`))
+
+export default app
